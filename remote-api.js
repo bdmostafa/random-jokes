e3cd104a27255973c38fad4a1de312b7c222c7af
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-13 13:47:49 +0600

    random jokes functionality

--- remote-api.js
+++ remote-api.js
@@ -5,11 +5,20 @@
 // DELETE
 
 // Selectors
-
+const jokesBtn = document.querySelector('#jokes');
+const categoriesBtn = document.querySelector('#categories');
+const displayJokes = document.querySelector('#jokes-area');
+const ulElement = document.querySelector('#category-ul');
 
 
 // Event Listeners
+jokesBtn.addEventListener('click', getJokes);
 
 
 
 // Functions
+async function getJokes() {
+    const jokes = await fetch('https://api.chucknorris.io/jokes/random')
+        .then(joke => joke.json());
+    displayJokes.textContent = jokes.value;
+}
