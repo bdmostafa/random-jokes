98df78430266ae355212ac8e3b703179f970a8b9
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-13 13:51:36 +0600

    categories-jokes functionality

--- remote-api.js
+++ remote-api.js
@@ -1,8 +1,5 @@
-// GET
-// POST
-// PUT
-// PATCH
-// DELETE
+// https://api.chucknorris.io/
+// Only GET request accepts
 
 // Selectors
 const jokesBtn = document.querySelector('#jokes');
@@ -13,7 +10,8 @@ const ulElement = document.querySelector('#category-ul');
 
 // Event Listeners
 jokesBtn.addEventListener('click', getJokes);
-
+categoriesBtn.addEventListener('click', getCategories);
+ulElement.addEventListener('click', getCategoryJokes);
 
 
 // Functions
@@ -21,4 +19,22 @@ async function getJokes() {
     const jokes = await fetch('https://api.chucknorris.io/jokes/random')
         .then(joke => joke.json());
     displayJokes.textContent = jokes.value;
+}
+
+async function getCategories() {
+    const categories = await fetch('https://api.chucknorris.io/jokes/categories')
+        .then(category => category.json());
+    let listHTML = '';
+    categories.forEach(category => {
+        listHTML += `<li>${category}</li>`;
+    })
+    ulElement.innerHTML = listHTML;
+}
+
+async function getCategoryJokes(e) {
+    // console.log(e.target)
+    const category = e.target.innerText;
+    const categoryJokes = await fetch(`https://api.chucknorris.io/jokes/random?${category}=${category}`)
+        .then(categoryJoke => categoryJoke.json());
+    displayJokes.textContent = categoryJokes.value;
 }
