6e436c08983acdf929f4f77f589ab30a2197d2b3
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-14 08:38:55 +0600

    updated on getMArked() functionality - CSS on click event

--- remote-api.js
+++ remote-api.js
@@ -6,7 +6,8 @@ const jokesBtn = document.querySelector('#jokes');
 const categoriesBtn = document.querySelector('#categories');
 const displayJokes = document.querySelector('#jokes-area');
 const ulElement = document.querySelector('#category-ul');
-
+let isJokesClicked = false;
+let isCategoryClicked = false;
 
 // Event Listeners
 jokesBtn.addEventListener('click', getJokes);
@@ -16,12 +17,18 @@ ulElement.addEventListener('click', getCategoryJokes);
 
 // Functions
 async function getJokes() {
+    isCategoryClicked = false;
+    isJokesClicked = true;
+    getMarked();
     const jokes = await fetch('https://api.chucknorris.io/jokes/random')
         .then(joke => joke.json());
     displayJokes.textContent = jokes.value;
+
 }
 
 async function getCategories() {
+    isJokesClicked = false;
+    getMarked();
     const categories = await fetch('https://api.chucknorris.io/jokes/categories')
         .then(category => category.json());
     let listHTML = '';
@@ -33,8 +40,30 @@ async function getCategories() {
 
 async function getCategoryJokes(e) {
     // console.log(e.target)
+    isJokesClicked = false;
+    isCategoryClicked = true;
+    getMarked(e);
     const category = e.target.innerText;
     const categoryJokes = await fetch(`https://api.chucknorris.io/jokes/random?${category}=${category}`)
         .then(categoryJoke => categoryJoke.json());
     displayJokes.textContent = categoryJokes.value;
+    isCategoryClicked = true;
+
+}
+
+// CSS design on button click event
+function getMarked(e) {
+    // Remove 'marked' class on all li elements at first, when any category is clicked
+    ulElement.childNodes.forEach(li => li.classList.remove('marked'));
+    // e parameter is found, when Categories is clicked otherwise 'else' executes on jokesBtn section
+    if (e) {
+        jokesBtn.classList.remove('marked');
+        // Find targeted click event li
+        const targetLi = e.target;
+        if (!isCategoryClicked) targetLi.classList.remove('marked')
+        else targetLi.classList.add('marked')
+    } else {
+        if (!isJokesClicked) jokesBtn.classList.remove('marked');
+        else jokesBtn.classList.add('marked');
+    }
 }
